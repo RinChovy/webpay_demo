ee982e2b1c1a84394348162394f3ab13a0bc8e5d
zx_lujingce <lujingce@bosssoft.com.cn>
2021-01-28 16:22:48 +0800

    liaoning.pc home request

--- src/liaoning.pc/pages/home/index.js
+++ src/liaoning.pc/pages/home/index.js
@@ -1,74 +1,55 @@
-import React from "react";
-import { Button } from "antd";
-import style from "./css/index.css";
-import { api } from "../../service/api.js";
+import React from 'react';
+import style from './css/index.css';
+
+import { queryHomelist } from '../../service/services';
 
 class Home extends React.Component {
   state = {
     list: [],
-    yunwei: "运维联系方式：024-22835015", //运维信息
+    yunwei: '运维联系方式：024-22835015', //运维信息
   };
 
   componentDidMount() {
-    let data = "regionCode=" + 210000 + "&password=" + 210000;
-    fetch(api.common, {
-      method: "post", //改成post
-      mode: "cors", //跨域
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        regionCode: 210000,
-      },
-      body: data, //向服务器发送的数据
-    })
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
+    queryHomelist({ regionCode: 210000 }).then(res => {
+      if (res.code === 0) {
         this.setState({
           list: JSON.parse(res.data.businessModels),
         });
-        console.log(this.state.list);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+      }
+    });
   }
 
   router = (url, code) => {
-    if(code==110){
-      window.location.href = url
-    }else{
+    if (code == 110) {
+      window.location.href = url;
+    } else {
       let data = { itemCode: code };
       this.props.history.push({ pathname: `/${url}`, query: data });
     }
   };
-  routers = (url) => {
-    this.props.history.push({ pathname: "/success", query: "太对了" });
+  routers = url => {
+    this.props.history.push({ pathname: '/success', query: '太对了' });
   };
 
   render() {
-    const { list,yunwei} = this.state;
+    const { list, yunwei } = this.state;
     const listModel = list.map((v, k) => {
       return (
-        <div className="box" key={k}>
+        <div className='box' key={k}>
           <div
-            className="box_on"
-            onClick={this.router.bind(
-              this,
-              `${list[k].modelUrl}`,
-              `${list[k].itemCode}`
-            )}
+            className='box_on'
+            onClick={this.router.bind(this, `${list[k].modelUrl}`, `${list[k].itemCode}`)}
           >
-            <img className="img-size" src={list[k].modelPicture} />
-            <span className="img-span">{list[k].itemName}</span>
+            <img className='img-size' src={list[k].modelPicture} />
+            <span className='img-span'>{list[k].itemName}</span>
           </div>
         </div>
       );
     });
     return (
       <div>
-        <div className="outForm">
-          <div className="onForm">
+        <div className='outForm'>
+          <div className='onForm'>
             {listModel}
             <div className='home_span_warn'>
               <span>{yunwei}</span>
---
+++ src/liaoning.pc/service/services.js
@@ -0,0 +1,14 @@
+import xFetch from '../../utils/xFetch';
+import { stringify } from 'qs';
+import { api } from './api.js';
+
+// home request
+export async function queryHomelist(params) {
+  return xFetch(api.common, {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+      regionCode: 210000,
+    },
+    data: stringify(params),
+  });
+}
