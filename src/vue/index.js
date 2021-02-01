79e89c14e06225ba6300f8aef97cf20113302ddb
zx_lujingce <lujingce@bosssoft.com.cn>
2021-02-01 15:32:25 +0800

    support vant

--- src/vue/index.js
+++ src/vue/index.js
@@ -1,21 +1,21 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-import App from "./index.vue";
-import Entry from "./pages/entry.vue";
-import About from "./pages/about.vue";
+import App from './index.vue';
+import Entry from './pages/entry.vue';
+import About from './pages/about.vue';
 
 window.enableDev = true;
 
 const routes = [
   {
-    path: "/",
+    path: '/',
     component: Entry,
   },
   {
-    path: "/about",
+    path: '/about',
     component: About,
   },
 ];
@@ -23,5 +23,5 @@ const router = new VueRouter({ routes });
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
--- src/vue/pages/entry.vue
+++ src/vue/pages/entry.vue
@@ -1,9 +1,22 @@
 <template>
-  <div class="mainwrap">This is vue entry page!</div>
+  <div class="mainwrap">
+    <p>This is vue entry page!</p>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+  </div>
 </template>
 
 <script>
-export default {};
+import {Button} from 'vant';
+export default {
+  name: "entry",
+  components: {
+    "van-button": Button,
+  }
+};
 </script>
 
 <style scoped lang="scss">
