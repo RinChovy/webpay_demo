df0f380760a66bfb70b4afaf01c2ccd7261e41dc
zx_lujingce <lujingce@bosssoft.com.cn>
2021-02-01 10:29:35 +0800

    vue demo

---
+++ src/vue/config/api.js
@@ -0,0 +1,4 @@
+export default {
+  // 导出
+  exportInfo: `${baseapi}/record/exportInfo`
+};
---
+++ src/vue/pages/entry.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mainwrap">This is vue entry page!</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped lang="scss">
+.mainwrap {
+  padding: 10px;
+}
+</style>
