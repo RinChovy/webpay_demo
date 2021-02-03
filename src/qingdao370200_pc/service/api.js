8be146664670505cde2155777ce3b250a33d4765
zx_lujingce <lujingce@bosssoft.com.cn>
2021-02-03 17:44:35 +0800

    qingdao api

--- src/qingdao370200_pc/service/api.js
+++ src/qingdao370200_pc/service/api.js
@@ -1,12 +1,12 @@
-const base = "http://192.168.5.15:8701/webpay_new/";
-// const base = "http://debug.epayservice.cn/webpay_new/"
+// const base = "http://192.168.5.15:8701/webpay_new/";
+const base = 'http://debug.epayservice.cn/webpay_new/';
 
 const api = {
-  common: base + "common/", //首页接口
-  getCo: base + "common/getCo", //验证码接口
-  queryPayInfo: base+ "common/queryPayInfo.do", //缴款书提交接口
-  createCharge: base+ "common/createCharge", //缴费接口
-  success: base+"callback/frontnotice", //成功回调接口
+  common: base + 'common/', //首页接口
+  getCo: base + 'common/getCo', //验证码接口
+  queryPayInfo: base + 'common/queryPayInfo.do', //缴款书提交接口
+  createCharge: base + 'common/createCharge', //缴费接口
+  success: base + 'callback/frontnotice', //成功回调接口
 };
 
 export { api };
