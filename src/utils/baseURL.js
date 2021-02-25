6bebc62c9f9451fa0f43ea2c8244d53707ef05de
zx_lujingce <lujingce@bosssoft.com.cn>
2021-02-25 17:23:23 +0800

    update baseURL

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,11 +1,13 @@
 /**
  * baseURL 映射
  */
-// let baseURL = 'https://debug.epayservice.cn/webpay_new';
-let baseURL = "http://192.168.5.15:8701/webpay_new";
 
-if (process.env.NODE_ENV === "production") {
-  baseURL = "https://api.epayservice.cn/webpay_new";
+let baseURL = '';
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'https://debug.epayservice.cn/webpay_new';
+} else if (process.env.NODE_ENV === 'production') {
+  baseURL = 'https://api.epayservice.cn/webpay_new';
 }
 
 export default baseURL;
