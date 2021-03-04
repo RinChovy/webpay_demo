3488cbc78a46236699915ccd8610f2369d8f1bbe
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-04 20:24:05 +0800

    baseapi

--- src/370200_m/config/api.js
+++ src/370200_m/config/api.js
@@ -1,9 +1,9 @@
-import baseapi from "utils/baseURL";
+import baseapi from 'utils/baseURL';
 
-const callback = baseapi + "/370200_m/index.html#/success"; //传给后台的回调地址
-const region = "000000"; //区划号
+const callback = 'https://dev.epayservice.cn/370200_m/success'; //传给后台的回调地址
+const region = '000000'; //区划号
 const regular = /^21\d{18}$/; //非税缴款正则表达式
-const regularText = "缴款码必须等于20位且37开头"; //非税缴款正则表达式提示信息
+const regularText = '缴款码必须等于20位且37开头'; //非税缴款正则表达式提示信息
 
 export default {
   //非税缴款正则表达式
@@ -21,7 +21,7 @@ export default {
   // 提交缴款书
   queryPayInfo: `${baseapi}/common/queryPayInfo.do`,
   // 缴费接口
-  createCharge: `${baseapi}/common/createChargePhone`,
+  createCharge: `${baseapi}/370200/createChargePhone`,
   //成功回调接口
   success: `${baseapi}/callback/frontnotice`,
   //支付宝接口1
