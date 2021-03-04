6b7b30aaae43cf0cb6298aaa042927d3b6692a3c
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-04 14:16:27 +0800

    route mode history

--- src/230400_m/index.js
+++ src/230400_m/index.js
@@ -1,47 +1,61 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-import App from "./index.vue";
-import Home from "./pages/home/index.vue";
-import Index_pay from "./pages/index_pay/index.vue";
-import Index_charge from "./pages/Index_charge/index.vue";
-import Success from "./pages/success/index.vue";
-import Fail from "./pages/fail/index.vue";
+import App from './index.vue';
+import Home from './pages/home/index.vue';
+import Index_pay from './pages/index_pay/index.vue';
+import Index_charge from './pages/Index_charge/index.vue';
+import Success from './pages/success/index.vue';
+import Fail from './pages/fail/index.vue';
+import Error404 from './pages/error404.vue';
 
 window.enableDev = true;
 
 const routes = [
   {
-    path: "/",
+    path: '/',
+    // component: Home,
+    redirect: '/home',
+  },
+  {
+    path: '/home', //首页
+    name: 'home',
     component: Home,
   },
   {
-    path: "/home", //首页
-    component: Home,
-  },
-  {
-    path: "/index_pay", //非税缴款界面
+    path: '/index_pay', //非税缴款界面
+    name: 'index_pay',
     component: Index_pay,
   },
   {
-    path: "/index_charge", //缴款书界面
-    name: "Index_charge",
+    path: '/index_charge', //缴款书界面
+    name: 'index_charge',
     component: Index_charge,
   },
   {
-    path: "/success", //成功回调页
+    path: '/success', //成功回调页
+    name: 'success',
     component: Success,
   },
   {
-    path: "/fail", //失败回调页
+    path: '/fail', //失败回调页
+    name: 'fail',
     component: Fail,
   },
+  {
+    path: '*',
+    component: Error404,
+  },
 ];
-const router = new VueRouter({ routes });
+const router = new VueRouter({
+  mode: 'history',
+  base: '/230400_m',
+  routes,
+});
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
