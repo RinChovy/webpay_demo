8628c607dfa98293c04e9e858906a1416a4ff486
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-04 15:46:45 +0800

    Update api.js

--- src/370200_p/service/api.js
+++ src/370200_p/service/api.js
@@ -1,23 +1,23 @@
-import base from "utils/baseURL";
+import base from 'utils/baseURL';
 
-const callback = base + "/370200_p/index.html#/success"; //传给后台的回调地址
+const callback = base + '/370200_p/index.html#/success'; //传给后台的回调地址
 
-const region = 370200; //区划号
+const region = '000000'; //区划号
 
 const regular = /^37\d{18}$/; //非税缴款正则表达式
 
-const regularText = "缴款码必须等于20位且37开头"; //非税缴款正则表达式提示信息
+const regularText = '缴款码必须等于20位且37开头'; //非税缴款正则表达式提示信息
 
 const api = {
   callback: callback, //传给后台的回调地址
   region: region, //区划号
   regular: regular, //非税缴款正则表达式
   regularText: regularText, //非税缴款正则表达式提示信息
-  common: base + "/common/", //首页接口
-  getCo: base + "/common/getCo", //验证码接口
-  queryPayInfo: base + "/common/queryPayInfo.do", //缴款书提交接口
-  createCharge: base + "/common/createCharge", //缴费接口
-  success: base + "/callback/frontnotice", //成功回调接口
+  common: base + '/common/', //首页接口
+  getCo: base + '/common/getCo', //验证码接口
+  queryPayInfo: base + '/common/queryPayInfo.do', //缴款书提交接口
+  createCharge: base + '/common/createCharge', //缴费接口
+  success: base + '/callback/frontnotice', //成功回调接口
 };
 
 export { api };
