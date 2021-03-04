18bf07c5e48df56bd804d66a1c6df01c8ff4f634
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-04 14:23:06 +0800

    route update

--- src/370200_m/index.js
+++ src/370200_m/index.js
@@ -1,60 +1,72 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
-import App from "./index.vue";
-import Home from "./pages/home/index.vue";
-import Acityservice from "./pages/acityservice/index.vue";
-import Index_pay from "./pages/index_pay/index.vue";
-import Index_charge from "./pages/Index_charge/index.vue";
-import Success from "./pages/success/index.vue";
-import Fail from "./pages/fail/index.vue";
-import Order_record from "./pages/order_record/index.vue";
+import App from './index.vue';
+import Home from './pages/home/index.vue';
+import Acityservice from './pages/acityservice/index.vue';
+import Index_pay from './pages/index_pay/index.vue';
+import Index_charge from './pages/Index_charge/index.vue';
+import Success from './pages/success/index.vue';
+import Fail from './pages/fail/index.vue';
+import Order_record from './pages/order_record/index.vue';
+import Error404 from './pages/error404.vue';
 
 window.enableDev = true;
 
 const routes = [
   {
-    path: "/",
+    path: '/',
+    redirect: '/home',
+  },
+  {
+    path: '/home', //首页
+    name: 'home',
     component: Home,
   },
   {
-    path: "/home", //首页
-    component: Home,
-  },
-  {
-    path: "/index_pay", //非税缴款界面
+    path: '/index_pay', //非税缴款界面
+    name: 'index_pay',
     component: Index_pay,
   },
   {
-    path: "/index_charge", //缴款书界面
-    name: "Index_charge",
+    path: '/index_charge', //缴款书界面
+    name: 'index_charge',
     component: Index_charge,
   },
   {
-    path: "/success", //成功回调页
+    path: '/success', //成功回调页
+    name: 'success',
     component: Success,
   },
   {
-    path: "/fail", //失败回调页
+    path: '/fail', //失败回调页
+    name: 'fail',
     component: Fail,
   },
   {
-    path: "/order_record", //微信小程序页
+    path: '/order_record', //微信小程序页
+    name: 'order_record',
     component: Order_record,
   },
   {
-    path: "/acityservice", //支付宝小程序页
+    path: '/acityservice', //支付宝小程序页
+    name: '/acityservice',
     component: Acityservice,
+  },
+  {
+    path: '*',
+    component: Error404,
   },
 ];
 const router = new VueRouter({
-  // mode: "history",
+  mode: 'history',
+  base: '/370200_m',
   routes,
 });
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
