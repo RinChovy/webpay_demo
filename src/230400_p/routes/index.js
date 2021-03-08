1cf0738ae4313a82df2c8aadf7e04faf22d300d3
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-08 14:55:57 +0800

    add .do

--- src/230400_p/routes/index.js
+++ src/230400_p/routes/index.js
@@ -1,22 +1,24 @@
-import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../pages/home/index.js";
-import NTpayChange from "../pages/index_charge/index.js";
-import success from "../pages/success/index.js";
-import fail from "../pages/fail/index.js";
+import React from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../pages/home/index.js';
+import NTpayChange from '../pages/index_charge/index.js';
+import success from '../pages/success/index.js';
+import fail from '../pages/fail/index.js';
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact={true} component={Home} />
+        <Route path='/' exact={true} component={Home} />
         {/* 非税缴款 */}
-        <Route path="/home" exact={true} component={Home} />
+        <Route path='/home' component={Home} />
+        <Route path='/index_pay' component={Home} />
+        <Route path='/index_pay.do' component={Home} />
         {/* 非税缴款缴款书界面 */}
-        <Route path="/index_charge" component={NTpayChange} />
+        <Route path='/index_charge' component={NTpayChange} />
         {/* 回调成功界面 */}
-        <Route path="/success" component={success} />
+        <Route path='/success' component={success} />
         {/* 回调失败界面 */}
-        <Route path="/fail" component={fail} />
+        <Route path='/fail' component={fail} />
       </Switch>
     </Router>
   );
