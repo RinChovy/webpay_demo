785a4c71c4e36e649cba6623ecdf2f44555d06c6
zx_lujingce <lujingce@bosssoft.com.cn>
2021-03-16 18:00:30 +0800

    mode history fix

--- config/dev.js
+++ config/dev.js
@@ -1,11 +1,12 @@
 const Bundler = require('parcel-bundler');
 const mockMiddleware = require('mock').middleware;
-const app = require('express')();
+const express = require('express');
 const parseArgs = require('minimist')(process.argv.slice(2));
 const project = parseArgs._[0];
 const chalk = require('chalk');
 const opn = require('opn');
 const getPort = require('get-port');
+const app = express();
 
 if (!project) {
   console.log(chalk.red('缺少项目名称!'));
@@ -19,12 +20,13 @@ const options = {
   sourceMaps: true,
   hmrPort: 0,
   cache: false,
+  // publicUrl: `/${project}`,
 };
 
 const bundler = new Bundler(file, options);
 
-app.use(mockMiddleware);
 app.use(bundler.middleware());
+app.use(mockMiddleware);
 
 getPort({ port: [8080] }).then(port => {
   app.listen(port, () => {
