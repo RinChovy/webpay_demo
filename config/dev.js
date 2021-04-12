3287d678b9afbfbc78b46492dd5f1648136ae820
ailjc <lujingce@163.com>
2021-04-12 11:42:17 +0800

    back off

---
+++ config/dev.js
@@ -0,0 +1,35 @@
+const Bundler = require('parcel-bundler');
+const mockMiddleware = require('mock').middleware;
+const express = require('express');
+const parseArgs = require('minimist')(process.argv.slice(2));
+const project = parseArgs._[0];
+const chalk = require('chalk');
+const opn = require('opn');
+const getPort = require('get-port');
+const app = express();
+
+if (!project) {
+  console.log(chalk.red('缺少项目名称!'));
+  return false;
+}
+
+const file = `src/${project}/index.html`;
+
+const options = {
+  logLevel: 3,
+  sourceMaps: true,
+  hmrPort: 0,
+  cache: false,
+  // publicUrl: `/${project}`,
+};
+
+const bundler = new Bundler(file, options);
+
+app.use(bundler.middleware());
+app.use(mockMiddleware);
+
+getPort({ port: [8080] }).then(port => {
+  app.listen(port, () => {
+    opn(`http://localhost:${port}/`);
+  });
+});
