8e5fb9d0786878fe934265ef5ca9df9d33c6918c
wwd <243912281@qq.com>
2021-05-14 11:24:50 +0800

    no message

--- src/370200_m/pages/index_charge/index.vue
+++ src/370200_m/pages/index_charge/index.vue
@@ -88,8 +88,6 @@
 
 <script>
 import { Button, Row, Col, Search, Dialog } from 'vant'
-// 大写金额
-import { Arabia_to_Chinese } from '../../public/js/money'
 // 自动生成商户订单号
 import { guid } from '../../public/js/orderNo'
 import API from '../../config/api.js'
@@ -173,6 +171,8 @@ export default {
           charge_param: {
             openid: openid,
             b: 'b',
+            payCode: this.payCode,
+            paymentName: this.payer,
             regionCode: API.region,
             frontCallBackUrl: API.callback,
           }, //(可选，用户自定义参数，若存在自定义参数则会通过 POST 方法透传给 charge_url
@@ -191,8 +191,8 @@ export default {
           }, //控件参数，常用来传递缴款服务所需定义的内容，如，非税paycode直缴或传入相关缴费信息生成缴款书
           charge_url: API.createCharge, //商户服务端创建charge时的controller地址
           charge_param: {
-            a: 'a',
-            b: 'b',
+            payCode: this.payCode,
+            paymentName: this.payer,
             regionCode: API.region,
             frontCallBackUrl: API.callback,
           }, //(可选，用户自定义参数，若存在自定义参数则会通过 POST 方法透传给 charge_url
@@ -210,7 +210,7 @@ export default {
     time(date) {
       var fillDate = date
       var fillDateStr = fillDate.substr(0, 4) + '-' + fillDate.substr(4, 2) + '-' + fillDate.substr(6, 2)
-      return fillDateStr;
+      return fillDateStr
     },
   },
 }
