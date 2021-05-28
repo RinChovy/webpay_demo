b49e14a41a5d5983487cb444ee033fcb81ffff95
ailjc <lujingce@163.com>
2021-05-28 15:14:43 +0800

    dev & pro build config

--- .ailjc.config.js
+++ .ailjc.config.js
@@ -2,6 +2,11 @@ const parseArgs = require('minimist')(process.argv.slice(2));
 const project = parseArgs._[0];
 const isProduction = process.env.NODE_ENV === 'production';
 
+/**
+ * 针对服务配置构建文件
+ * 勿修改
+ */
+
 module.exports = {
   outDir: `dist/${project}`,
   sourceMaps: !isProduction,
